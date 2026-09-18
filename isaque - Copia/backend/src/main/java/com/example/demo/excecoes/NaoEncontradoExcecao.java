e8d730f8bc337188e855cddbd32abe76a5544672
Isaq-11 <isaquecarvalhoxavier@gmail.com>
2026-09-17 22:26:37 -0300

    Delete isaque - Copia directory

--- isaque - Copia/backend/src/main/java/com/example/demo/excecoes/NaoEncontradoExcecao.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.excecoes;
-
-public class NaoEncontradoExcecao extends RuntimeException {
-    public NaoEncontradoExcecao(String mensagem) {
-        super(mensagem);
-    }
-}
--- isaque - Copia/backend/src/main/java/com/example/demo/excecoes/NegocioExcecao.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.excecoes;
-
-public class NegocioExcecao extends RuntimeException {
-    public NegocioExcecao(String mensagem) {
-        super(mensagem);
-    }
-}
